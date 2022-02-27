26eacde646f47866cfe4862989d02a3c53ed65e7
Archie <44258193+a-edwaar@users.noreply.github.com>
2022-02-27 15:36:44 +0000

    MVP (#1)
    * prisma db setup
    
    * API routes
    
    * random user, list of comments & upvote
    
    * comment & upvote e2e tests

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ### Run locally
 
 1. `npm install` to install dependencies
-2. `npm start` to run the server on http://localhost:3000
+2. Add DATABASE_URL in a `.env` file and point it to your DB
+3. `npx prisma migrate dev` to sync your database with the project schema
+4. `npm start` to run the server on http://localhost:3000
--- app.ts
+++ app.ts
@@ -1,9 +1,107 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const db = new PrismaClient();
 const port = 3000;
 
+db.$connect();
+
+app.use(express.json());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+/*
+GET /api/comments - Get all comments
+*/
+app.get(
+  "/api/comments",
+  async (_req: express.Request, res: express.Response) => {
+    const comments = await db.comment.findMany();
+    res.send(comments);
+  }
+);
+
+/*
+POST /api/comments - Create a new comment
+*/
+app.post(
+  "/api/comments",
+  async (req: express.Request, res: express.Response) => {
+    const { name, avatarURL, content } = req.body;
+    console.log(name, avatarURL, content);
+    if (
+      typeof name !== "string" ||
+      typeof avatarURL !== "string" ||
+      typeof content !== "string"
+    ) {
+      return res.status(301).redirect("/?error=bad-request");
+    }
+    try {
+      const comment = await db.comment.create({
+        data: {
+          name,
+          avatarURL,
+          content,
+        },
+      });
+      console.log(`New comment with id: ${comment.id}`);
+      res.status(301).redirect("/");
+    } catch (e) {
+      console.error(e);
+      res.status(500).redirect("/?error=internal");
+    }
+  }
+);
+
+/*
+POST /api/comments/:id - Upvote a comment
+*/
+app.post(
+  "/api/comments/:id",
+  async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+    try {
+      const updatedComment = await db.comment.update({
+        where: {
+          id,
+        },
+        data: {
+          upvotes: {
+            increment: 1,
+          },
+        },
+      });
+      console.log(`Comment with id: ${updatedComment.id} has been upvoted`);
+      res.status(301).redirect("/");
+    } catch (e) {
+      console.error(e);
+      res.status(500).redirect("/?error=internal");
+    }
+  }
+);
+
+/*
+DELETE /api/comments/:id - Delete a comment for testing purposes
+*/
+app.delete(
+  "/api/comments/:id",
+  async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+    try {
+      await db.comment.delete({
+        where: {
+          id,
+        },
+      });
+      console.log(`Comment with id: ${id} has been deleted`);
+      res.send();
+    } catch (e) {
+      console.error(e);
+      res.status(501).send("Internal server error");
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
